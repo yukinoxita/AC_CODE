20224b480e3aa126a61f24e2ba504d02683a506c
yukinoxita <m1091057545@163.com>
2020-01-13 19:36:33 +0800

    xiuzheng

--- lqb/2017B/7.cpp
+++ lqb/2017B/7.cpp
@@ -3,14 +3,17 @@ int a[100010];
 int b[100010];
 int n,k;
 int minn = 0;
-int check(int num)
+bool check(int num)
 {
     int i,j;
     int cnt = 0;
     for(i=1;i<=n;i++)
+    {
         cnt += ((a[i]/num)*(b[i]/num));
+        if(cnt >= k)return true;
+    }
    // printf("num = %d, cnt = %d\n",num,cnt);
-    return cnt;
+    return 0;
 }
 int main()
 {
@@ -21,7 +24,7 @@ int main()
     for(i=1;i<=n;i++){scanf("%d%d",&a[i],&b[i]);if(minn < b[i])minn = b[i];if(minn < a[i])minn = a[i];}
     while(1)
     {
-        if(check(minn) >= k){printf("%d",minn);break;}
+        if(check(minn)){printf("%d",minn);break;}
         minn -- ;
     }
     return 0;
